4fbae946669fa23ba5c19f5570164c5c22cec43a
Bella <Isabellapinkerman@gmail.com>
2022-10-25 10:41:52 -0400

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const express = require("express");
+const postBank = require("./postBank");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => {
+  const posts = postBank.list()
+
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+  </head>
+  <body>
+    <ul>
+      ${posts.map(post=>`<li>${'data'}<li>`)}
+    </ul>
+  </body>
+  </html>`;
+
+  res.send(html)
+});
 
 const PORT = 1337;
 
