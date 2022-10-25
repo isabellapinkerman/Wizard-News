3ac3e2a146a07991e61b91037d08971b535dcbfe
Bella <Isabellapinkerman@gmail.com>
2022-10-25 12:11:20 -0400

    finished, working on bonus

--- app.js
+++ app.js
@@ -1,27 +1,110 @@
 const express = require("express");
 const postBank = require("./postBank");
 const app = express();
+const morgan = require('morgan');
+const path = require('path')
+
+function timeToEnglish(ms){
+  const days = ms/86400000
+  ms = ms%86400000
+  const hours = ms/3600000
+  ms = ms%3600000
+  const minute = ms/60000
+  ms = ms%60000
+  const seconds = ms/1000
+  ms = ms%1000
+
+  return `${days}, days, ${hours}, hours, ${minutes}, minutes, ${seconds}, seconds`
+}
+
+
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get("/", (req, res) => {
   const posts = postBank.list()
+  const time = Date.now()
 
   const html = `<!DOCTYPE html>
   <html>
   <head>
     <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
   </head>
   <body>
-    <ul>
-      ${posts.map(post=>`<li>${'data'}<li>`)}
-    </ul>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts.map(post => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span><a href='/posts/${post.id}'>${post.title}</a>
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${time - post.date}
+          </small>
+        </div>`
+      ).join('')}
+    </div>
   </body>
-  </html>`;
+</html>`;
 
   res.send(html)
 });
 
-const PORT = 1337;
 
+app.get('/posts/:id', (req, res)=>{
+const id = req.params.id;
+const post = postBank.find(id)
+let htmlString = `<!DOCTYPE html>
+<html>
+<head>
+  <title>${post.title}</title>
+  <link rel="stylesheet" href="/style.css" />
+</head>
+<body>
+  <div class="news-list">
+    <header><img src="/logo.png"/>Wizard News</header>
+    </span>${post.title}
+            <small>(by ${post.name})</small>
+    <p>${post.content}</p>
+    <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+  </div>
+</body>
+</html>`
+
+if(!post.id){
+ throw new Error('Not Found')
+}
+
+res.send(htmlString)
+})
+
+app.use((error, req, res, next)=>{
+  console.error(error.stack)
+  const htmlString =  `
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <header><img src="/logo.png"/>Wizard News</header>
+    <div class="not-found">
+      <p>Accio Page! 🧙‍♀️ ... Page Not Found</p>
+      <img src="/dumbledore-404.gif" />
+    </div>
+  </body>
+  </html>`
+
+  res.status(404).send(htmlString)
+})
+
+const PORT = 1337;
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
